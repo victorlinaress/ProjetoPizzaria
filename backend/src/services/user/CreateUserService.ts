22e4f3ccad4753dd6f58ca84098f5650c7bd0c5b
unknown <victorbr8@outlook.com>
2025-03-28 02:02:36 -0300

    user

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma";
+
 interface UserRequest {
     name: string;
     email: string;
@@ -7,7 +9,30 @@ interface UserRequest {
 class CreateUserService {
     async execute({ name, email, password }: UserRequest) {
         console.log(name);
-        return { name: name };
+
+        //verificar se enviou um email
+        if (!email){
+            throw new Error("Email Incorrect") 
+        }
+
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists")
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name:name,
+                email: email,
+                password: password
+            }
+        })
+        return user;
     }
 }
 
