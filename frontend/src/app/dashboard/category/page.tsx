44b1bd989124163a6c63c91977b0b014887b5519
unknown <victorbr8@outlook.com>
2025-04-27 18:08:29 -0300

    category

---
+++ frontend/src/app/dashboard/category/page.tsx
@@ -0,0 +1,53 @@
+import styles from "./styles.module.scss";
+import { Button } from "../components/button";
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
+import { getCookieServer } from "@/lib/cookieServer";
+
+export default function Category() {
+  async function handleRegisterCategory(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name");
+    if (!name || name === "") return;
+
+    const data = {
+      name: name,
+    };
+
+    const token = getCookieServer();
+
+    try {
+      const response = await api.post('/category', data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      // Depois que cadastrar, pode redirecionar, por exemplo:
+      redirect('/dashboard'); 
+      
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+  }
+
+  return (
+    <main className={styles.container}>
+      <h1>Página de categoria</h1>
+
+      <form className={styles.form} action={handleRegisterCategory}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome da categoria, ex: Pizzas, Refrigerante"
+          required
+          className={styles.input}
+        />
+
+        <Button name="Cadastrar" />
+      </form>
+    </main>
+  );
+}
